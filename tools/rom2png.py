6a31a5b0e1b19b93f622e46bae8e71c96ef614de
Daid <daid303@gmail.com>
2022-04-04 21:02:57 +0200

    Add script which generates a big image from a rom.

---
+++ tools/rom2png.py
@@ -0,0 +1,51 @@
+import argparse
+import PIL.Image
+import PIL.ImageDraw
+import numpy
+
+def twoBitsPerPixel(data, width, height=None):
+    if height is None:
+        height = len(data) // 16 // width
+    addr = 0
+    img = numpy.zeros((height * 8, width * 8), dtype=numpy.uint8)
+    for y in range(height):
+        for x in range(width):
+            for row in range(8):
+                a = data[addr]
+                b = data[addr + 1]
+                addr += 2
+                for col in range(8):
+                    v = 0
+                    if a & (0x80 >> col):
+                        v |= 1
+                    if b & (0x80 >> col):
+                        v |= 2
+                    img[y*8+row, x*8+col] = v
+    img = PIL.Image.fromarray(img, "P")
+    img.putpalette([0xc4,0xf0,0xc2, 0x5a,0xb9,0xa8, 0x1e,0x60,0x6e, 0x2d,0x1b,0x00])
+    return img
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("rom", type=str)
+    parser.add_argument("--output", type=str, required=True)
+    args = parser.parse_args()
+
+    data = open(args.rom, "rb").read()
+    bank_count = len(data) // 0x4000
+    
+    img = PIL.Image.new("P", (bank_count * 136 + 64, 512 + 32))
+    draw = PIL.ImageDraw.Draw(img)
+    img.putpalette([0xc4,0xf0,0xc2, 0x5a,0xb9,0xa8, 0x1e,0x60,0x6e, 0x2d,0x1b,0x00, 0x00,0x00,0x00, 0xFF,0xFF,0xFF])
+    draw.rectangle(((0,0), img.size), 4)
+    for addr in range(0, 0x8001, 0x0800):
+        y = addr * 512 // 0x8000 + 16
+        draw.text((3, y - 10), "%04X" % (addr), 5)
+        draw.text((bank_count * 136 + 32, y - 10), "%04X" % (addr), 5)
+        draw.line((0, y, img.size[0], y), 5)
+    for bank_nr in range(bank_count):
+        draw.text((64 + 16 + bank_nr * 136, 0), "Bank%02X" % (bank_nr), 5)
+        draw.text((64 + 16 + bank_nr * 136, 512 + 16), "Bank%02X" % (bank_nr), 5)
+        img.paste(twoBitsPerPixel(data[bank_nr*0x4000:bank_nr*0x4000+0x4000], 16), (32 + bank_nr * 136, 16))
+    img.save(args.output)
